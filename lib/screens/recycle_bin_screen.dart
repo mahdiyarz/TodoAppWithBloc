ebc57fea6b7cc75b105d29d24064f93003de623a
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-01-24 21:21:58 +0330

    Create recycle_bin_screen.dart

---
+++ lib/screens/recycle_bin_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class RecycleBinScreen extends StatelessWidget {
+  const RecycleBinScreen({super.key});
+  static const routeName = '/recycle-bin-screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
