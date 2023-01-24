0cbb4325fa56f9049b7574daa10731deab83f320
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-01-24 21:31:13 +0330

    Display first look of recycle bin screen

--- lib/screens/recycle_bin_screen.dart
+++ lib/screens/recycle_bin_screen.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/task_list_builder.dart';
+import 'drawer_screen.dart';
+
 class RecycleBinScreen extends StatelessWidget {
   const RecycleBinScreen({super.key});
   static const routeName = '/recycle-bin-screen';
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Recycle Bin is here'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      drawer: const DrawerScreen(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const [
+          Center(
+            child: Chip(
+              label: Text(
+                'Tasks: ',
+              ),
+            ),
+          ),
+          TasksListBuilder(tasksList: [])
+        ],
+      ),
+    );
   }
 }
