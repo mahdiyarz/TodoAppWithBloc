06f13b654b39fa3b81672c3e910fb60947bb009f
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-01-25 23:17:45 +0330

    Set task counter on drawer

--- lib/screens/drawer_screen.dart
+++ lib/screens/drawer_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todo_bloc/blocs/bloc_exports.dart';
+import 'package:todo_bloc/models/tasks_model.dart';
 
 import '../screens/recycle_bin_screen.dart';
 import '../screens/tasks_screen.dart';
@@ -27,18 +29,30 @@ class DrawerScreen extends StatelessWidget {
                 textAlign: TextAlign.center,
               ),
             ),
-            ListTile(
-              onTap: () =>
-                  Navigator.of(context).pushNamed(TasksScreen.routeName),
-              leading: const Icon(Icons.folder_special_rounded),
-              title: const Text('My Tasks List'),
+            BlocBuilder<TasksBloc, TasksState>(
+              builder: (context, state) {
+                List<TaskModel> tasksList = state.allTasks;
+                return ListTile(
+                  onTap: () =>
+                      Navigator.of(context).pushNamed(TasksScreen.routeName),
+                  leading: const Icon(Icons.folder_special_rounded),
+                  trailing: Text(tasksList.length.toString()),
+                  title: const Text('My Tasks List'),
+                );
+              },
             ),
             const Divider(),
-            ListTile(
-              onTap: () =>
-                  Navigator.of(context).pushNamed(RecycleBinScreen.routeName),
-              leading: const Icon(Icons.folder_delete_outlined),
-              title: const Text('Recycle Bin'),
+            BlocBuilder<TasksBloc, TasksState>(
+              builder: (context, state) {
+                List<TaskModel> removedList = state.removedTasks;
+                return ListTile(
+                  onTap: () => Navigator.of(context)
+                      .pushNamed(RecycleBinScreen.routeName),
+                  leading: const Icon(Icons.folder_delete_outlined),
+                  trailing: Text(removedList.length.toString()),
+                  title: const Text('Recycle Bin'),
+                );
+              },
             ),
           ],
         ),
