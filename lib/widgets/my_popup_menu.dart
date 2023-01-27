48a5c2f73320fcfabaefa644cfae1bbb494636fb
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-01-27 19:55:48 +0330

    Create my_popup_menu.dart

---
+++ lib/widgets/my_popup_menu.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:todo_bloc/models/tasks_model.dart';
+
+class MyPopupMenu extends StatelessWidget {
+  final TaskModel task;
+  final VoidCallback cancelOrDeleteCallback;
+  final VoidCallback likeOrDislikeCallback;
+  const MyPopupMenu({
+    Key? key,
+    required this.task,
+    required this.cancelOrDeleteCallback,
+    required this.likeOrDislikeCallback,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton(
+      itemBuilder: task.isDelete == false
+          ? (context) => [
+                PopupMenuItem(
+                  child: TextButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.edit),
+                    label: const Text('Edit'),
+                  ),
+                ),
+                PopupMenuItem(
+                  onTap: likeOrDislikeCallback,
+                  child: TextButton.icon(
+                    onPressed: null,
+                    icon: const Icon(Icons.bookmark),
+                    label: const Text('Add to bookmark'),
+                  ),
+                ),
+                PopupMenuItem(
+                  onTap: cancelOrDeleteCallback,
+                  child: TextButton.icon(
+                    onPressed: null,
+                    icon: const Icon(Icons.delete),
+                    label: const Text('Delete'),
+                  ),
+                ),
+              ]
+          : (context) => [
+                PopupMenuItem(
+                  child: TextButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.restore_from_trash),
+                    label: const Text('Restore task'),
+                  ),
+                ),
+                PopupMenuItem(
+                  child: TextButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.delete_forever),
+                    label: const Text('Delete forever'),
+                  ),
+                ),
+              ],
+    );
+  }
+}
